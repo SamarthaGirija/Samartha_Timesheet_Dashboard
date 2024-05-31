3d9ec8ec00f1b45600b20fb97b1249e1a45a2af6
SamarthaGirija <125431977+SamarthaGirija@users.noreply.github.com>
2024-05-31 10:29:00 +0530

    Java source code(31-05-2024)

---
+++ Java source code/src/main/java/com/samartha/LoginServlet.java
@@ -0,0 +1,56 @@
+package com.samartha;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/login")
+public class LoginServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+    private static final String JDBC_URL = "jdbc:mysql://localhost:3307/yd";
+    private static final String JDBC_USER = "root";
+    private static final String JDBC_PASSWORD = "root"; // Change to your MySQL root password
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection connection = DriverManager.getConnection(JDBC_URL, JDBC_USER, JDBC_PASSWORD);
+            String sql = "SELECT * FROM Users WHERE username = ? AND password = ?";
+            PreparedStatement statement = connection.prepareStatement(sql);
+            statement.setString(1, username);
+            statement.setString(2, password);
+            ResultSet resultSet = statement.executeQuery();
+
+            if (resultSet.next()) {
+                response.sendRedirect("index.html");
+            } else {
+                out.println("<h1>Invalid username or password</h1>");
+            }
+
+            resultSet.close();
+            statement.close();
+            connection.close();
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+            out.println("<h1>Error occurred: " + e.getMessage() + "</h1>");
+        } finally {
+            out.close();
+        }
+    }
+}
